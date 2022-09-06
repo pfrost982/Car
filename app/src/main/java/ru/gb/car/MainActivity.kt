e1e684b2bf39ca6b9fcbc783650618bdb29bc8bb
pfrost982 <pfrost982@gmail.com>
2022-09-06 23:05:28 +0700

    car simple move to point

--- app/src/main/java/ru/gb/car/MainActivity.kt
+++ app/src/main/java/ru/gb/car/MainActivity.kt
@@ -7,12 +7,22 @@ import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.gb.car.databinding.ActivityMainBinding
+import ru.gb.car.entity.Car
+import ru.gb.car.entity.Point
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var surfaceHolder: SurfaceHolder? = null
-    lateinit var carBitmap: Bitmap
+
+    private lateinit var carBitmap: Bitmap
+    private lateinit var pointBitmap: Bitmap
+
+    private val car = Car()
+    private val point = Point()
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +35,10 @@ class MainActivity : AppCompatActivity() {
         binding.surface.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
                 this@MainActivity.surfaceHolder = surfaceHolder
+                val canvas =surfaceHolder.lockCanvas()
+                drawCar(canvas)
+                surfaceHolder.unlockCanvasAndPost(canvas)
+
             }
 
             override fun surfaceChanged(surfaceHolder: SurfaceHolder, i: Int, i1: Int, i2: Int) {
@@ -36,20 +50,20 @@ class MainActivity : AppCompatActivity() {
             }
         })
         carBitmap = BitmapFactory.decodeResource(resources, R.drawable.car)
-
+        pointBitmap = BitmapFactory.decodeResource(resources, R.drawable.point)
 
         binding.surface.setOnTouchListener { _, motionEvent ->
             if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                point.x = motionEvent.x
+                point.y = motionEvent.y
+                carToPoint(surfaceHolder)
+/*
                 val canvas = surfaceHolder?.lockCanvas()
                 if (canvas != null) {
-                    drawCar(
-                        canvas,
-                        motionEvent.x,
-                        motionEvent.y,
-                        (0..360).random().toFloat()
-                    )
+                    render(canvas)
                     surfaceHolder?.unlockCanvasAndPost(canvas)
                 }
+*/
                 return@setOnTouchListener true
             } else {
                 return@setOnTouchListener false
@@ -57,17 +71,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun drawCar(canvas: Canvas, x: Float, y: Float, angle: Float) {
+    private fun carToPoint(surfaceHolder: SurfaceHolder?) {
+        GlobalScope.launch {
+            val dx = (point.x - car.x) / 100
+            val dy = (point.y - car.y) / 100
+            for (i in 1..100) {
+                val canvas = surfaceHolder?.lockCanvas()
+                if (canvas != null) {
+                    render(canvas)
+                    surfaceHolder?.unlockCanvasAndPost(canvas)
+                }
+                car.x += dx
+                car.y += dy
+                delay(100)
+            }
+        }
+    }
+
+    private fun render(canvas: Canvas){
         canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+        drawCar(canvas)
+        drawPoint(canvas)
+    }
+
+    private fun drawCar(canvas: Canvas) {
         val matrix = Matrix()
         matrix.postScale(0.1f, 0.1f)
-        matrix.postRotate(angle)
+        matrix.postRotate(car.angle)
 
         val rotatedBitmap =
             Bitmap.createBitmap(carBitmap, 0, 0, carBitmap.width, carBitmap.height, matrix, false)
         canvas.drawBitmap(
             rotatedBitmap,
-            x - rotatedBitmap.width.toFloat() / 2, y - rotatedBitmap.height.toFloat() / 2, null
+            car.x - rotatedBitmap.width.toFloat() / 2, car.y - rotatedBitmap.height.toFloat() / 2, null
+        )
+    }
+
+    private fun drawPoint(canvas: Canvas) {
+        val matrix = Matrix()
+        matrix.postScale(0.1f, 0.1f)
+
+        val rotatedBitmap =
+            Bitmap.createBitmap(pointBitmap, 0, 0, pointBitmap.width, pointBitmap.height, matrix, false)
+        canvas.drawBitmap(
+            rotatedBitmap,
+            point.x - rotatedBitmap.width.toFloat() / 2, point.y - rotatedBitmap.height.toFloat() / 2, null
         )
     }
 }
---
+++ app/src/main/java/ru/gb/car/entity/Point.kt
@@ -0,0 +1,6 @@
+package ru.gb.car.entity
+
+class Point {
+    var x: Float = 100f
+    var y: Float = 100f
+}
