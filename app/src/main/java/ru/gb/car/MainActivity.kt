e44260e04122cf4ae7ccee437c15a3882115e21b
pfrost982 <pfrost982@gmail.com>
2022-09-15 21:04:19 +0700

    full screen

--- app/src/main/java/ru/gb/car/MainActivity.kt
+++ app/src/main/java/ru/gb/car/MainActivity.kt
@@ -3,9 +3,11 @@ package ru.gb.car
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
 import android.graphics.PixelFormat
+import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.SurfaceHolder
+import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -22,6 +24,15 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContentView(binding.root)
 
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         binding.surface.setZOrderOnTop(true)
         binding.surface.holder.setFormat(PixelFormat.TRANSPARENT)
